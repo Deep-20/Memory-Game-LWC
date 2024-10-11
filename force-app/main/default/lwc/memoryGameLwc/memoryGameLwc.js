55b8959471599161691f6032f73006ac2f905852
Deep-20 <49397575+Deep-20@users.noreply.github.com>
2024-10-11 11:55:14 -0400

    Completed the Game

--- force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
+++ force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
@@ -4,6 +4,13 @@ import FONT_ASWESOME from '@salesforce/resourceUrl/fontawesome'
 export default class MemoryGameLwc extends LightningElement {
 
     isLibLoaded = false;
+    openedCards = []
+    matchedCards = []
+    moves = 0
+    timerRef
+    totalTime='00:00'
+    showCongratulations = false
+
     cards = [
         {id:1, listClass:"card", type:"diamond", icon:"fa fa-diamond"},
         {id:2, listClass:"card", type:"plane", icon:"fa fa-paper-plane-o"},
@@ -22,6 +29,114 @@ export default class MemoryGameLwc extends LightningElement {
         {id:15, listClass:"card", type:"plane", icon:"fa fa-paper-plane-o"},
         {id:16, listClass:"card", type:"cube", icon:"fa fa-cube"},
     ]
+
+    get gameRating(){
+        let stars = this.moves < 9 ? [1,2,3] : this.moves <= 13 ? [1,2] : [1]
+        return this.matchedCards.length === 16 ? stars : []
+    }
+
+    displayCard(event){
+        let curCard = event.target;
+        curCard.classList.add("open", "show", "disabled");
+        this.openedCards = this.openedCards.concat(event.target);
+        const len = this.openedCards.length;
+        if(len === 2){
+            this.moves = this.moves+1
+            if(this.moves === 1){
+                this.timer()
+            }
+            if(this.openedCards[0].type === this.openedCards[1].type){
+                this.matchedCards = this.matchedCards.concat(this.openedCards[0], this.openedCards[1])
+                this.matched()    
+            }else{
+                this.unmatched()
+            }
+        }
+    }
+
+    matched(){
+        this.openedCards[0].classList.add("match", "disabled")
+        this.openedCards[1].classList.add("match", "disabled")
+        this.openedCards[0].classList.remove("show", "open")
+        this.openedCards[1].classList.remove("show", "open")
+        this.openedCards = []
+        if(this.matchedCards.length === 16){
+            window.clearInterval(this.timerRef)
+            this.showCongratulations = true
+
+        }
+    }
+
+    unmatched(){
+        this.openedCards[0].classList.add("unmatched")
+        this.openedCards[1].classList.add("unmatched")
+        this.action('DISABLE')
+        setTimeout(() => {
+            this.openedCards[0].classList.remove("show", "open", "unmatched")
+            this.openedCards[1].classList.remove("show", "open", "unmatched")
+            this.action('ENABLE')
+            this.openedCards = []
+        }, 1100)
+    }
+
+    action(action){
+        let cards = this.template.querySelectorAll('.card')
+        Array.from(cards).forEach(item => {
+            if(action === 'ENABLE'){
+                let isMatch = item.classList.contains('match')
+                if(!isMatch){
+                    item.classList.remove('disabled');
+                }
+            }
+            // else{
+            //     item.classList.add('disabled');
+            // }
+
+            if(action === 'DISABLE'){
+                item.classList.add('disabled');
+            }
+        });
+    }
+
+    timer(){
+        let startTime = new Date();
+        this.timerRef = setInterval(() => {
+            let diff = new Date().getTime() - startTime.getTime()
+            let d = Math.floor(diff/1000)
+            const m = Math.floor(d % 3600 / 60)
+            const s = Math.floor(d % 3600 % 60)
+            const mDisplay = m>0 ? m+(m===1 ? "minute, " : " minutes, "): ""
+            const sDisplay = s>0 ? s+(s===1 ? "second" : " seconds"): ""
+            this.totalTime = mDisplay + sDisplay
+        }, 1000)
+    }
+
+    shuffle(){
+        this.openedCards = []
+        this.matchedCards = []
+        this.totalTime = '00:00'
+        this.moves=0
+        window.clearInterval(this.timerRef)
+        this.showCongratulations = false
+        let elem = this.template.querySelectorAll('.card')
+        Array.from(elem).forEach(item => {
+            item.classList.remove('show', 'open', 'match', 'disabled')
+        })
+
+        // Shuffling logic
+        let array = [...this.cards]
+        let counter = array.length
+        while(counter > 0){
+            let index = Math.floor(Math.random()*counter)
+            counter--
+
+            let temp = array[counter]
+            array[counter] = array[index]
+            array[index] = temp
+        }
+
+        this.cards = [...array]
+    }
 
 
     renderedCallback(){
